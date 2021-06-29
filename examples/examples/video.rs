bcc8e9f60faf84a324f9978599971640a3088630
Junha Yang <junhayang1@gmail.com>
2021-06-29 22:52:02 +0900

    Implement basics of video example

---
+++ examples/examples/video.rs
@@ -0,0 +1,171 @@
+#![allow(clippy::many_single_char_names)]
+
+use fere::prelude::{fere_resources::Texture, *};
+use fere_window::*;
+use rand::prelude::*;
+use rayon::prelude::*;
+use std::sync::Arc;
+
+struct Scene {
+    renderer: Fere,
+    world: Option<Box<World>>,
+    resources: Arc<Resources>,
+}
+
+struct Resources {
+    triangle: Arc<Texture>,
+}
+
+#[derive(Clone, Debug)]
+struct Particle {
+    pos: Vec3,
+    speed: Vec3,
+    acc: Vec3,
+    color: IVec3,
+    size: f32,
+}
+
+struct World {
+    particles: Vec<Particle>,
+    time: usize,
+}
+
+fn gen_color(x: f32) -> IVec3 {
+    let r = (x * 0.9 + 0.4).cos() + 1.0;
+    let b = (x * 0.8 + 0.3).cos() + 1.0;
+    let g = (x * 0.7 + 0.2).cos() + 1.0;
+    let r = 128 + (127.0 * r * 0.5) as i32;
+    let g = 128 + (127.0 * g * 0.5) as i32;
+    let b = 128 + (127.0 * b * 0.5) as i32;
+    IVec3::new(r, g, b)
+}
+
+fn cos(x: f32) -> f32 {
+    x.cos()
+}
+
+impl World {
+    fn new() -> Self {
+        let mut rng = thread_rng();
+        Self {
+            particles: Vec::new(),
+            time: rng.gen_range(0..100),
+        }
+    }
+
+    fn update(&mut self) {
+        let params = [(1.0, 2.0), (3.0, 2.0), (4.0, 1.0), (2.0, 2.0)];
+
+        if self.time % 10 == 0 {
+            let t = self.time as f32 * 0.001;
+
+            let sp_pos: Vec<_> = params
+                .iter()
+                .map(|(a, b)| {
+                    let a = *a;
+                    let b = *b;
+                    let x = cos(cos(t * 0.4) * (cos(a) + 1.1)) * 100.0
+                        + cos(cos(t * 0.3 * cos(b))) * 10.0;
+                    let y = cos(cos(t * 0.4) * (cos(b) + 1.1)) * 100.0
+                        + cos(cos(t * 0.3 * cos(a))) * 10.0;
+                    let z = 0.0;
+                    Vec3::new(x, y, z)
+                })
+                .collect();
+
+            for sp in sp_pos {
+                for i in 0..30 {
+                    let angle = ((t as i32 + (i as i32) * 2) as f32).to_radians();
+                    let speed = Vec3::new(angle.cos(), angle.sin(), 0.0) * 1.3;
+                    self.particles.push(Particle {
+                        pos: sp,
+                        speed,
+                        acc: Vec3::new(0.0, 0.0, 0.0),
+                        color: gen_color(t),
+                        size: 0.5,
+                    });
+                }
+            }
+        }
+        self.particles.par_iter_mut().for_each(|x| {
+            x.speed += x.acc;
+            x.pos += x.speed;
+        });
+
+        self.particles.retain(|x| length(&x.pos) < 500.0);
+        println!("{}", self.particles.len());
+        self.time += 1;
+    }
+}
+
+impl Program for Scene {
+    fn new() -> Self {
+        let renderer = Fere::new(
+            serde_yaml::from_str(
+                &std::fs::read_to_string("./examples/examples/basic_fere_configs.yml").unwrap(),
+            )
+            .unwrap(),
+        );
+        let world = Some(Box::new(World::new()));
+        Scene {
+            renderer,
+            resources: Arc::new(Resources {
+                triangle: fere_examples::read_texture("triangle.png"),
+            }),
+            world,
+        }
+    }
+
+    fn update(&mut self) -> String {
+        let cpos = Vec3::new(0.0, 0.0, 300.0);
+        let screen_size = self.renderer.configs().resolution;
+        let mut camera = SetCamera::new(
+            cpos,
+            Vec3::new(0.0, 0.0, 0.0),
+            Vec3::new(0.0, 1.0, 0.0),
+            (90.0_f32).to_radians(),
+            screen_size.x as f32 / screen_size.y as f32,
+            0.1,
+            1000.0,
+        );
+        camera.trans();
+
+        let (frame, renderer) = self.renderer.new_frame(FrameConfig {
+            camera,
+            show_lightvolume_outline: false,
+        });
+
+        let resources = Arc::clone(&self.resources);
+        self.world.as_mut().unwrap().update();
+        let world = self.world.take().unwrap();
+
+        let render_thread = std::thread::spawn(|| render(frame, resources, world));
+        self.renderer.end_frame(renderer.render());
+        self.world = Some(render_thread.join().unwrap());
+        "continue".to_owned()
+    }
+}
+
+fn render(mut frame: Frame, resources: Arc<Resources>, world: Box<World>) -> Box<World> {
+    for p in &world.particles {
+        frame.push(rops::DrawBillboard {
+            texture: Arc::clone(&resources.triangle),
+            pos: p.pos,
+            size: Vec2::from_element(p.size),
+            rotation: f32::atan2(p.speed.y, p.speed.x),
+            blend_mode: (),
+            color: nalgebra::convert::<IVec3, Vec3>(p.color) / 255.0,
+        });
+    }
+    frame.end();
+    world
+}
+
+fn main() {
+    run::<Scene>(
+        serde_yaml::from_str(
+            &std::fs::read_to_string("./examples/examples/window_config.yml").unwrap(),
+        )
+        .unwrap(),
+    );
+}
